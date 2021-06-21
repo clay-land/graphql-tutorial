b4a928d9adc65f31aa1f60dec58dba02bcda26cf
Clayton Landry <clayton.landry@gmail.com>
2021-06-21 10:16:56 -0400

    authentication complete

---
+++ db/migrate/20210621141332_add_user_id_link.rb
@@ -0,0 +1,4 @@
+class AddUserIdLink < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_023803) do
+ActiveRecord::Schema.define(version: 2021_06_21_141332) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
